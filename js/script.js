b652b1e530fc0bafaf5b439fb6618975064d67d9
pearmeow <perryhuang2015@gmail.com>
2025-06-02 13:01:43 -0400

    Complete basic operation functions in js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
 console.log("The script is running");
+
+function add(x = 0, y = 0) {
+    return x + y;
+}
+function sub(x = 0, y = 0) {
+    return x - y;
+}
+function mul(x = 0, y = 0) {
+    return x * y;
+}
+function div(x = 0, y = 0) {
+    if (y = 0) return "ERROR";
+    return x / y;
+}
+
+function operate(first = "", operation = "", second = "") {
+    switch (operation) {
+        case ("+"):
+            return add(+first, +second);
+        case ("-"):
+            return sub(+first, +second);
+        case ("*"):
+            return mul(+first, +second);
+        case ("/"):
+            return div(+first, +second);
+    }
+    return "ERROR";
+}
+
+let first = "";
+let operation = "";
+let second = "";
+
