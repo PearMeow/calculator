609b35bd9f5b056121905bd483427a5ad519dd72
pearmeow <perryhuang2015@gmail.com>
2025-06-03 08:43:23 -0400

    Organize html elements into groups for js later

--- js/script.js
+++ js/script.js
@@ -10,10 +10,11 @@ function mul(x = 0, y = 0) {
     return x * y;
 }
 function div(x = 0, y = 0) {
-    if (y = 0) return "ERROR";
+    if (y = 0) return "ERR_DIV_BY_0";
     return x / y;
 }
 
+// evaluates if equal or another operation is pressed while first, operation, and second exist
 function operate(first = "", operation = "", second = "") {
     switch (operation) {
         case ("+"):
@@ -25,10 +26,18 @@ function operate(first = "", operation = "", second = "") {
         case ("/"):
             return div(+first, +second);
     }
-    return "ERROR";
+    return "ERR_NO_OP";
 }
 
 let first = "";
 let operation = "";
 let second = "";
+let decimal = false;
 
+
+
+// on operation: run operate if conditions met, otherwise replace operation unless op is equals
+// on AC: reset first, operation, and second to ""
+// on DEL: pop last char from current operand if not empty, otherwise do nothing
+// on num: append num to current operand
+// on decimal: if decimal exists, do nothing, othewise if operand empty append 0., otherwise append .
