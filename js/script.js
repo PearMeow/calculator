29462ad56f871cb54d90c18d908af17b1f8c2609
pearmeow <perryhuang2015@gmail.com>
2025-06-03 11:50:25 -0400

    Refactor callback into function & fix negative del edge case

--- js/script.js
+++ js/script.js
@@ -49,51 +49,75 @@ const nums = document.querySelectorAll(".num");
 const operations = document.querySelectorAll(".op");
 const deletion = document.querySelectorAll(".deletion");
 
-for (const op of operations) {
-    op.addEventListener("click", () => {
-        const theOp = op.textContent;
-        if (first !== "" && operation !== "" && second !== "") {
-            first = "" + operate(first, operation, second);
-            if (theOp !== "=") {
-                operation = theOp;
-            } else { // if theOp === "="
-                operation = "";
-            }
-            second = "";
-            calcDisplay.textContent = first;
-            if (first === "ERR_DIV_BY_0" || first === "ERR_OVERFLOW") first = "";
-            currNum = FIRST;
-            if (first.includes(".")) {
-                decimal = true;
-            }
-        } else if (first !== "" && theOp !== "=") {
+function doOperation(theOp = "") {
+    if (first !== "" && operation !== "" && second !== "") {
+        first = "" + operate(first, operation, second);
+        if (theOp !== "=") {
             operation = theOp;
-            currNum = SECOND;
-            calcDisplay.textContent = second;
-            decimal = false;
+        } else { // if theOp === "="
+            operation = "";
         }
-    })
+        second = "";
+        calcDisplay.textContent = first;
+        if (first === "ERR_DIV_BY_0" || first === "ERR_OVERFLOW") first = "";
+        currNum = FIRST;
+        if (first.includes(".")) {
+            decimal = true;
+        }
+    } else if (first !== "" && theOp !== "=") {
+        operation = theOp;
+        currNum = SECOND;
+        calcDisplay.textContent = second;
+        decimal = false;
+    }
 }
 
-for (const num of nums) {
-    num.addEventListener("click", () => {
-        let theNum = (currNum === FIRST) ? first : second;
-        if (theNum.length === 13) return;
-        if (num.textContent === "." && decimal === true) return;
-        if (theNum.length === 1 && num.textContent !== ".") {
-            if (theNum[0] === "0") {
-                theNum = num.textContent;
-            } else {
-                theNum += num.textContent;
-            }
-        } else if (num.textContent === "." && decimal === false) {
-            if (theNum.length === 0) {
-                theNum += "0";
-            }
-            theNum += ".";
-            decimal = true;
+function appendToNum(appendee = "") {
+    let theNum = (currNum === FIRST) ? first : second;
+    if (theNum.length === 13) return;
+    if (appendee === "." && decimal === true) return;
+    if (theNum.length === 1 && appendee !== ".") {
+        if (theNum[0] === "0") {
+            theNum = appendee;
         } else {
-            theNum += num.textContent;
+            theNum += appendee;
+        }
+    } else if (appendee === "." && decimal === false) {
+        if (theNum.length === 0) {
+            theNum += "0";
+        }
+        theNum += ".";
+        decimal = true;
+    } else {
+        theNum += appendee;
+    }
+    if (currNum === FIRST) {
+        first = theNum;
+        calcDisplay.textContent = first;
+    } else { // if currNum === SECOND
+        second = theNum;
+        calcDisplay.textContent = second;
+    }
+}
+
+function deleteNum(mode = "") {
+    if (mode === "AC") {
+        first = "";
+        operation = "";
+        second = "";
+        decimal = false;
+        currNum = FIRST;
+        calcDisplay.textContent = first;
+    } else { // if mode === "DEL"
+        let theNum = (currNum === FIRST) ? first : second;
+        if (theNum.length === 0) return;
+        if (theNum[theNum.length - 1] === ".") {
+            decimal = false;
+        }
+        if (theNum.length === 2 && theNum[0] === "-") {
+            theNum = 0;
+        } else {
+            theNum = theNum.slice(0, theNum.length - 1);
         }
         if (currNum === FIRST) {
             first = theNum;
@@ -102,32 +126,17 @@ for (const num of nums) {
             second = theNum;
             calcDisplay.textContent = second;
         }
-    });
+    }
+}
+
+for (const op of operations) {
+    op.addEventListener("click", () => doOperation(op.textContent));
+}
+
+for (const num of nums) {
+    num.addEventListener("click", () => appendToNum(num.textContent));
 }
 
 for (const del of deletion) {
-    del.addEventListener("click", () => {
-        if (del.textContent === "AC") {
-            first = "";
-            operation = "";
-            second = "";
-            decimal = false;
-            currNum = FIRST;
-            calcDisplay.textContent = first;
-        } else { // if del.textContent === "DEL"
-            let theNum = (currNum === FIRST) ? first : second;
-            if (theNum.length === 0) return;
-            if (theNum[theNum.length - 1] === ".") {
-                decimal = false;
-            }
-            theNum = theNum.slice(0, theNum.length - 1);
-            if (currNum === FIRST) {
-                first = theNum;
-                calcDisplay.textContent = first;
-            } else { // if currNum === SECOND
-                second = theNum;
-                calcDisplay.textContent = second;
-            }
-        }
-    });
+    del.addEventListener("click", () => deleteNum(del.textContent));
 }
